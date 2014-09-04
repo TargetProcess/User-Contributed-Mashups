e222450a1fefc15f5f66176eabee64f906592276
Phil Hazelton <philhaz@gmail.com>
2014-09-04 15:48:04 +0100

    Updated sidebar acronyms for compatibility with 3.3.0, removed other broken features

--- MiniSidebarLabels/MiniSidebarLabels.js
+++ MiniSidebarLabels/MiniSidebarLabels.js
@@ -14,29 +14,24 @@ tau.mashups
  * Docs: http://docs.jquery.com/Plugins/livequery
  */
 (function(a){a.extend(a.fn,{livequery:function(e,d,c){var b=this,f;if(a.isFunction(e)){c=d,d=e,e=undefined}a.each(a.livequery.queries,function(g,h){if(b.selector==h.selector&&b.context==h.context&&e==h.type&&(!d||d.$lqguid==h.fn.$lqguid)&&(!c||c.$lqguid==h.fn2.$lqguid)){return(f=h)&&false}});f=f||new a.livequery(this.selector,this.context,e,d,c);f.stopped=false;f.run();return this},expire:function(e,d,c){var b=this;if(a.isFunction(e)){c=d,d=e,e=undefined}a.each(a.livequery.queries,function(f,g){if(b.selector==g.selector&&b.context==g.context&&(!e||e==g.type)&&(!d||d.$lqguid==g.fn.$lqguid)&&(!c||c.$lqguid==g.fn2.$lqguid)&&!this.stopped){a.livequery.stop(g.id)}});return this}});a.livequery=function(b,d,f,e,c){this.selector=b;this.context=d;this.type=f;this.fn=e;this.fn2=c;this.elements=[];this.stopped=false;this.id=a.livequery.queries.push(this)-1;e.$lqguid=e.$lqguid||a.livequery.guid++;if(c){c.$lqguid=c.$lqguid||a.livequery.guid++}return this};a.livequery.prototype={stop:function(){var b=this;if(this.type){this.elements.unbind(this.type,this.fn)}else{if(this.fn2){this.elements.each(function(c,d){b.fn2.apply(d)})}}this.elements=[];this.stopped=true},run:function(){if(this.stopped){return}var d=this;var e=this.elements,c=a(this.selector,this.context),b=c.not(e);this.elements=c;if(this.type){b.bind(this.type,this.fn);if(e.length>0){a.each(e,function(f,g){if(a.inArray(g,c)<0){a.event.remove(g,d.type,d.fn)}})}}else{b.each(function(){d.fn.apply(this)});if(this.fn2&&e.length>0){a.each(e,function(f,g){if(a.inArray(g,c)<0){d.fn2.apply(g)}})}}}};a.extend(a.livequery,{guid:0,queries:[],queue:[],running:false,timeout:null,checkQueue:function(){if(a.livequery.running&&a.livequery.queue.length){var b=a.livequery.queue.length;while(b--){a.livequery.queries[a.livequery.queue.shift()].run()}}},pause:function(){a.livequery.running=false},play:function(){a.livequery.running=true;a.livequery.run()},registerPlugin:function(){a.each(arguments,function(c,d){if(!a.fn[d]){return}var b=a.fn[d];a.fn[d]=function(){var e=b.apply(this,arguments);a.livequery.run();return e}})},run:function(b){if(b!=undefined){if(a.inArray(b,a.livequery.queue)<0){a.livequery.queue.push(b)}}else{a.each(a.livequery.queries,function(c){if(a.inArray(c,a.livequery.queue)<0){a.livequery.queue.push(c)}})}if(a.livequery.timeout){clearTimeout(a.livequery.timeout)}a.livequery.timeout=setTimeout(a.livequery.checkQueue,20)},stop:function(b){if(b!=undefined){a.livequery.queries[b].stop()}else{a.each(a.livequery.queries,function(c){a.livequery.queries[c].stop()})}}});a.livequery.registerPlugin("append","prepend","after","before","wrap","attr","removeAttr","addClass","removeClass","toggleClass","empty","remove","html");a(function(){a.livequery.play()})})(jQuery);
-  
-// iteration highlight
-  $('.time-left').livequery(function() {
-    var target = $(this).closest('[data-x]').attr('data-x');
-    $('[data-x="'+target+'"]').css('background', '#FFF');
-  });
-  
-// title labels
-  $('.tau-label__text').livequery(function() {
-    $(this).attr('title', $(this).text());
-  });
-    
-// board acronyms
+
   $('head').append('<style>'
-  +  '.tau-sp-collapsed .tau-boardselector__item:before {'
-  +    'background: none;'
-  +    'color: #686D72;'
+  +  '.tau-sp-collapsed .t3-views-navigator .i-role-board-menu-item .t3-header:before {'
+  +    'background-image: none;'
+  +    'color: #8598AC;'
   +    'content: attr(data-content);'
   +    'font-weight: bold;'
   +    'text-align: center;'
-  +    'top: 17px;'
-  +    'left: 0;'
+  +    'top: 0;'
+  +    'text-indent: -24px;'
+  +    'font-size: 12px;'
+  +    'line-height: 10px;'
   +    'width: 40px;'
+  +  '}'
+  +  '.tau-sp-collapsed .t3-views-navigator .i-role-board-menu-item:hover .t3-header:before {'
+  +    'background-image: url(https://css.cdntpondemand.com/3_3_0_9858/tau/css.board/images/icons-general.svg?v=3.3.0.9858);'
+  +    'content: "";'
+  +  '}'
   +'</style>');
   
   function acronym(text) {
@@ -45,9 +40,10 @@ tau.mashups
     for (var i = 0, l = words.length; i < l; i++) result += words[i].substr(0, 1);
     return result.toUpperCase();
   }
-    
-  $('.tau-boardselector__item').livequery(function() {
-    var label = acronym($(this).text());
+  
+  $('.t3-views-navigator .i-role-board-menu-item .t3-header').livequery(function() {
+    var text = $(this).text();
+    var label = (text.length > 3) ? acronym(text) : text;
     $(this).attr('data-content', label);
   });
   
